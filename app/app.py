4cc34e422e63cd68b71aed067227959b3c179bba
Justas Zieminykas <justaxsz@gmail.com>
2025-04-10 17:26:37 +0300

    Wired the flask main tracker route to display the main page and updated all URLs in the template to send the selected arguments for filter and goal to the route

--- app/app.py
+++ app/app.py
@@ -14,9 +14,18 @@ from flask import (
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(32)
 
-@app.route('/tracker', methods=['GET'])
-def show_summary():
-    return render_template('tracker', goal='lose', filter='weeks');
+
+@app.route('/')
+def home():
+    return redirect('/tracker')
+
+@app.route('/tracker', methods=['GET','POST'])
+def tracker():
+    print(request.args)
+    # TODO: Validate filter params. If not within available options, use default values.
+    goal = request.args.get('goal', 'lose')
+    filter = request.args.get('filter', 'weeks')
+    return render_template('tracker.html', goal=goal, filter=filter)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5040)
