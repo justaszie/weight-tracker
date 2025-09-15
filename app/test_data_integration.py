5af873a92778c35dd756aa19ad2bce34b29a35ae
Justas Zieminykas <justaxsz@gmail.com>
2025-09-15 14:38:19 +0300

    Recovered unit test files from the flask project branch which was decomissioned

---
+++ app/test_data_integration.py
@@ -0,0 +1,285 @@
+import datetime as dt
+import pytest
+
+from google_fit import NoCredentialsError
+from data_integration import (
+    DataIntegrationService,
+    DataSyncError,
+    SourceNoDataError,
+    SourceFetchError,
+)
+from file_storage import FileStorage
+
+
+@pytest.fixture
+def service(mocker):
+    return DataIntegrationService(mocker.Mock(), mocker.Mock())
+
+
+@pytest.fixture
+def sample_raw_data():
+    return {"data": {"2020-01-02": 78.56}}
+
+
+@pytest.fixture
+def sample_daily_entries():
+    return [
+        {"date": dt.date(2025, 8, 28), "weight": 73.6},
+        {"date": dt.date(2025, 8, 29), "weight": 73.6},
+        {"date": dt.date(2025, 9, 1), "weight": 73.0},
+    ]
+
+
+def test_refresh_no_raw_data_found(mocker, service):
+    mocker.patch(
+        "data_integration.DataIntegrationService.convert_to_daily_entries",
+        side_effect=SourceNoDataError(),
+    )
+
+    with pytest.raises(SourceNoDataError):
+        service.refresh_weight_entries()
+
+    service.source.get_raw_data.assert_called_once()
+
+
+@pytest.mark.parametrize(
+    "test_entries",
+    [
+        [
+            {"date": dt.date(2025, 8, 28), "weight": 73.6},
+            {"date": dt.date(2025, 8, 29), "weight": 73.6},
+        ],
+        [
+            {"date": dt.date(2025, 8, 28), "weight": 73.6},
+        ],
+        [
+            {"date": dt.date(2025, 8, 28), "weight": 70.1},
+            {"date": dt.date(2025, 8, 29), "weight": 70.2},
+            {"date": dt.date(2025, 9, 1), "weight": 73.0},
+        ],
+    ],
+)
+def test_refresh_no_new_data(mocker, service, sample_daily_entries, test_entries):
+    mocker.patch(
+        "data_integration.DataIntegrationService.convert_to_daily_entries"
+    ).return_value = test_entries
+    mocker.patch(
+        "data_integration.DataIntegrationService.get_existing_weight_entries"
+    ).return_value = sample_daily_entries
+    mock_store_data = mocker.patch(
+        "data_integration.DataIntegrationService.store_new_weight_entries"
+    )
+
+    new_entries = service.refresh_weight_entries()
+
+    mock_store_data.assert_called_once_with([])
+    assert new_entries == []
+
+
+@pytest.mark.parametrize(
+    "test_entries, expected_new_entries",
+    [
+        (
+            [
+                {"date": dt.date(2025, 9, 20), "weight": 70.9},
+            ],
+            [
+                {"date": dt.date(2025, 9, 20), "weight": 70.9},
+            ],
+        ),
+        (
+            [
+                {"date": dt.date(2025, 1, 5), "weight": 70.9},
+                {"date": dt.date(2025, 8, 28), "weight": 70.9},
+                {"date": dt.date(2025, 8, 29), "weight": 70.9},
+                {"date": dt.date(2025, 8, 30), "weight": 72.9},
+                {"date": dt.date(2025, 9, 28), "weight": 71.9},
+            ],
+            [
+                {"date": dt.date(2025, 1, 5), "weight": 70.9},
+                {"date": dt.date(2025, 8, 30), "weight": 72.9},
+                {"date": dt.date(2025, 9, 28), "weight": 71.9},
+            ],
+        ),
+    ],
+)
+def test_refresh_new_data(
+    mocker, service, sample_daily_entries, test_entries, expected_new_entries
+):
+    mocker.patch(
+        "data_integration.DataIntegrationService.convert_to_daily_entries"
+    ).return_value = test_entries
+    mocker.patch(
+        "data_integration.DataIntegrationService.get_existing_weight_entries"
+    ).return_value = sample_daily_entries
+    mock_store_data = mocker.patch(
+        "data_integration.DataIntegrationService.store_new_weight_entries"
+    )
+
+    new_entries = service.refresh_weight_entries()
+
+    mock_store_data.assert_called_once_with(expected_new_entries)
+    assert new_entries == expected_new_entries
+
+
+def test_refresh_with_store_raw_copy_on(mocker, service):
+    mocker.patch("data_integration.DataIntegrationService.get_raw_data")
+    mocker.patch("data_integration.DataIntegrationService.convert_to_daily_entries")
+    mocker.patch("data_integration.DataIntegrationService.get_existing_weight_entries")
+    mocker.patch("data_integration.DataIntegrationService.filter_new_weight_entries")
+    mocker.patch("data_integration.DataIntegrationService.store_new_weight_entries")
+    mock_store_raw_copy_fn = mocker.patch(
+        "data_integration.DataIntegrationService.store_raw_data"
+    )
+    mock_store_csv_copy_fn = mocker.patch.object(service.storage, "export_to_csv")
+
+    service.refresh_weight_entries(store_csv_copy=False, store_raw_copy=True)
+
+    mock_store_raw_copy_fn.assert_called_once()
+    assert not mock_store_csv_copy_fn.called
+
+
+def test_refresh_with_store_csv_on(mocker, service):
+    mocker.patch("data_integration.DataIntegrationService.get_raw_data")
+    mocker.patch("data_integration.DataIntegrationService.convert_to_daily_entries")
+    mocker.patch("data_integration.DataIntegrationService.get_existing_weight_entries")
+    mocker.patch("data_integration.DataIntegrationService.filter_new_weight_entries")
+    mocker.patch("data_integration.DataIntegrationService.store_new_weight_entries")
+    mock_store_raw_copy_fn = mocker.patch(
+        "data_integration.DataIntegrationService.store_raw_data"
+    )
+    mock_store_csv_copy_fn = mocker.patch.object(service.storage, "export_to_csv")
+
+    service.refresh_weight_entries(store_csv_copy=True, store_raw_copy=False)
+
+    mock_store_csv_copy_fn.assert_called_once()
+    assert not mock_store_raw_copy_fn.called
+
+
+def test_get_raw_data(mocker, service, sample_raw_data):
+    mock_source_fetch_fn = mocker.patch.object(service.source, "get_raw_data")
+    mock_source_fetch_fn.return_value = sample_raw_data
+    assert service.get_raw_data() == sample_raw_data
+
+
+def test_get_raw_data_credentials_error(mocker, service):
+    mock_source_fetch_fn = mocker.patch.object(service.source, "get_raw_data")
+    mock_source_fetch_fn.side_effect = NoCredentialsError()
+
+    with pytest.raises(SourceFetchError):
+        raw_data = service.get_raw_data()
+
+
+def test_get_raw_data_other_errors(mocker, service):
+    mock_source_fetch_fn = mocker.patch.object(service.source, "get_raw_data")
+    mock_source_fetch_fn.side_effect = Exception()
+
+    with pytest.raises(SourceFetchError):
+        raw_data = service.get_raw_data()
+
+
+def test_store_raw_data(mocker, service, sample_raw_data):
+    mock_source_fn = mocker.patch.object(service.source, "store_raw_data")
+    service.store_raw_data(sample_raw_data)
+    mock_source_fn.assert_called_once_with(sample_raw_data)
+
+
+# Test NoDataError,
+def test_convert_to_daily_entries(
+    mocker, service, sample_raw_data, sample_daily_entries
+):
+    mock_source_fn = mocker.patch.object(service.source, "convert_to_daily_entries")
+    mock_source_fn.return_value = sample_daily_entries
+
+    result = service.convert_to_daily_entries(sample_raw_data)
+
+    mock_source_fn.assert_called_once_with(sample_raw_data)
+    assert result == sample_daily_entries
+
+
+def test_convert_to_daily_entries_empty_dataset(mocker, service):
+    mock_source_fn = mocker.patch.object(service.source, "convert_to_daily_entries")
+    mock_source_fn.return_value = []
+    test_raw_data = {}
+
+    with pytest.raises(SourceNoDataError):
+        result = service.convert_to_daily_entries(test_raw_data)
+
+    mock_source_fn.assert_called_once_with(test_raw_data)
+
+
+def test_get_existing_weight_entries(mocker, service, sample_daily_entries):
+    mock_storage_fn = mocker.patch.object(service.storage, "get_weight_entries")
+    mock_storage_fn.return_value = sample_daily_entries
+
+    result = service.get_existing_weight_entries()
+
+    mock_storage_fn.assert_called_once()
+    assert result == sample_daily_entries
+
+
+@pytest.mark.parametrize(
+    "test_entries, expected_filtered_entries",
+    [
+        (
+            [
+                {"date": dt.date(2025, 9, 20), "weight": 70.9},
+            ],
+            [
+                {"date": dt.date(2025, 9, 20), "weight": 70.9},
+            ],
+        ),
+        (
+            [
+                {"date": dt.date(2025, 8, 28), "weight": 70.9},
+                {"date": dt.date(2025, 8, 29), "weight": 70.9},
+            ],
+            [],
+        ),
+        (
+            [
+                {"date": dt.date(2025, 1, 5), "weight": 70.9},
+                {"date": dt.date(2025, 8, 28), "weight": 70.9},
+                {"date": dt.date(2025, 8, 29), "weight": 70.9},
+                {"date": dt.date(2025, 8, 30), "weight": 72.9},
+                {"date": dt.date(2025, 9, 28), "weight": 71.9},
+            ],
+            [
+                {"date": dt.date(2025, 1, 5), "weight": 70.9},
+                {"date": dt.date(2025, 8, 30), "weight": 72.9},
+                {"date": dt.date(2025, 9, 28), "weight": 71.9},
+            ],
+        ),
+    ],
+)
+def test_filter_new_weight_entries(
+    mocker, service, test_entries, sample_daily_entries, expected_filtered_entries
+):
+    mocker.patch.object(service, "get_existing_weight_entries").return_value = (
+        sample_daily_entries
+    )
+
+    filtered_new_entries = service.filter_new_weight_entries(test_entries)
+
+    assert filtered_new_entries == expected_filtered_entries
+
+
+def test_store_new_weight_entries(mocker, service, sample_daily_entries):
+    mock_storage_fn = mocker.patch.object(service.storage, "create_weight_entry")
+    sample_entry_date, sample_entry_weight = sample_daily_entries[0].values()
+
+    service.store_new_weight_entries(sample_daily_entries)
+
+    assert mock_storage_fn.call_count == len(sample_daily_entries)
+    mock_storage_fn.assert_any_call(sample_entry_date, sample_entry_weight)
+
+
+def test_store_new_weight_entries_with_persist_command(mocker, sample_daily_entries):
+    service = DataIntegrationService(FileStorage(), mocker.Mock())
+
+    mock_storage_fn = mocker.patch.object(service.storage, "create_weight_entry")
+    mock_persist_fn = mocker.patch.object(service.storage, "save")
+
+    service.store_new_weight_entries(sample_daily_entries)
+
+    mock_persist_fn.assert_called_once()
---
+++ app/test_google_fit.py
@@ -0,0 +1,308 @@
+import datetime as dt
+import json
+import pytest
+
+from google_fit import (
+    GoogleFitAuth,
+    GoogleFitClient,
+    HttpError,
+    NoCredentialsError,
+    RAW_DATA_FILE_PATH,
+    TOKEN_FILE_PATH,
+)
+
+"""
+ def __init__(self, creds: Credentials | None) -> None
+#
+#     def convert_to_daily_entries(self, raw_data: Any) -> list[DailyWeightEntry]
+#         # Normalizes raw API data into [{'date': date, 'weight': float}, ...]
+#         # Keeps last weight per day, filters weight outliers (50 < w < 100).
+#
+#     def _is_empty_dataset(self, dataset: Any) -> bool
+#         # Internal helper: True if dataset missing 'point' or empty.
+ """
+
+@pytest.fixture
+def client_no_creds():
+    return GoogleFitClient()
+
+@pytest.fixture
+def client():
+    return GoogleFitClient("testcreds")
+
+
+@pytest.fixture
+def sample_raw_dataset():
+    return {
+        "minStartTimeNs": "0",
+        "maxEndTimeNs": "1757050801266618112",
+        "dataSourceId": "derived:com.google.weight:com.google.android.gms:merge_weight",
+        "point": [
+            {
+                "startTimeNanos": "1756960320000000000",
+                "endTimeNanos": "1756960320000000000",
+                "dataTypeName": "com.google.weight",
+                "originDataSourceId": "raw:com.google.weight:com.google.android.apps.fitness:user_input",
+                "value": [{"fpVal": 73.0999984741211, "mapVal": []}],
+                "modifiedTimeMillis": "1756960338250",
+            },
+            {
+                "startTimeNanos": "1756874340000000000",
+                "endTimeNanos": "1756874340000000000",
+                "dataTypeName": "com.google.weight",
+                "originDataSourceId": "raw:com.google.weight:com.google.android.apps.fitness:user_input",
+                "value": [{"fpVal": 73.5599984741211, "mapVal": []}],
+                "modifiedTimeMillis": "1756874454792",
+            },
+        ],
+    }
+
+
+@pytest.fixture
+def empty_raw_dataset():
+    return {
+        "minStartTimeNs": "0",
+        "maxEndTimeNs": "1757050801266618112",
+        "dataSourceId": "derived:com.google.weight:com.google.android.gms:merge_weight",
+        "point": [],
+    }
+
+@pytest.fixture
+def auth_client():
+    return GoogleFitAuth()
+
+@pytest.fixture
+def test_token_file_data():
+        return """
+        {
+            "token": "abca12",
+            "refresh_token": "1//tjj5",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "client_id": "aaa.apps.googleusercontent.com",
+            "client_secret": "add444",
+            "scopes": [
+                "https://www.googleapis.com/auth/fitness.body.read",
+                "https://www.googleapis.com/auth/fitness.activity.read"
+            ],
+            "universe_domain": "googleapis.com",
+            "account": "",
+            "expiry": "2025-09-08T07:06:53.565408Z"
+        }"""
+
+
+
+def test_get_raw_data_no_creds(client_no_creds):
+    with pytest.raises(NoCredentialsError):
+        result = client_no_creds.get_raw_data()
+
+
+def test_get_raw_data(mocker, client, sample_raw_dataset):
+    # Setting up test parameters to the multiple function calls
+    date_from_ns_timestamp = 0
+    test_now_timestamp = dt.datetime.now()
+    tomorrow: dt.datetime = test_now_timestamp + dt.timedelta(days=1)
+    date_to_ns_timestamp = int(tomorrow.timestamp() * 1e9)
+    user_id_param = "me"
+    data_source_id_param = (
+        "derived:com.google.weight:com.google.android.gms:merge_weight"
+    )
+    dataset_id_param = f"{date_from_ns_timestamp}-{date_to_ns_timestamp}"
+
+    get_requets_params = {
+        "userId": user_id_param,
+        "dataSourceId": data_source_id_param,
+        "datasetId": dataset_id_param,
+    }
+
+    # Mocking the datetime to compare fixed param values
+    mocker.patch("google_fit.dt.datetime").now.return_value = test_now_timestamp
+
+    # Mocking the 3rd party function objects
+    mock_service_build_fn = mocker.patch("google_fit.build")
+    mock_service = mock_service_build_fn.return_value
+
+    mock_get_request_fn = mock_service.users().dataSources().datasets().get
+    mock_request = mock_get_request_fn.return_value
+
+    mock_execute_fn = mock_request.execute
+    mock_execute_fn.return_value = sample_raw_dataset
+
+    # Run the actual code
+    result = client.get_raw_data()
+
+    # Testing the function return value and the 3rd party function calls with expected params
+    mock_service_build_fn.assert_called_once_with(
+        "fitness", "v1", credentials=client.creds
+    )
+    mock_get_request_fn.assert_called_once_with(**get_requets_params)
+    mock_execute_fn.assert_called_once()
+    assert result == sample_raw_dataset
+
+
+def test_get_empty_raw_dataset(mocker, client, empty_raw_dataset):
+    mock_service = mocker.patch("google_fit.build").return_value
+    mock_execute_fn = (
+        mock_service.users().dataSources().datasets().get.return_value.execute
+    )
+    mock_execute_fn.return_value = empty_raw_dataset
+
+    result = client.get_raw_data()
+
+    mock_execute_fn.assert_called_once()
+    assert result == empty_raw_dataset
+
+def test_get_raw_data_api_http_errors(mocker, client):
+    mock_service = mocker.patch("google_fit.build").return_value
+    mock_execute_fn = (
+        mock_service.users().dataSources().datasets().get.return_value.execute
+    )
+
+    mock_err_response = mocker.Mock()
+    mock_err_response.status = 404
+    mock_execute_fn.side_effect=HttpError(mock_err_response, "Invalid Dataset".encode())
+
+    # get_raw_data expected to propagate HttpError
+    with pytest.raises(HttpError):
+        client.get_raw_data()
+
+def test_extract_datapoints(client_no_creds, sample_raw_dataset):
+    datapoints = client_no_creds._extract_datapoints(sample_raw_dataset)
+    expected_properties = ("value", "startTimeNanos", "endTimeNanos")
+    # Checking that the datapoints extracted from the response contains all the properties we use in conversion
+    assert type(datapoints) == list
+    assert len(datapoints) == 2
+
+    assert all(
+        [
+            all([key in datapoint for key in expected_properties])
+            and "fpVal" in datapoint["value"][0]
+            for datapoint in datapoints
+        ]
+    )
+
+def test_convert_to_daily_entries(client_no_creds, sample_raw_dataset):
+    daily_entries = client_no_creds.convert_to_daily_entries(sample_raw_dataset)
+    assert daily_entries == [
+        {
+            "date": dt.date(2025, 9, 3),
+            "weight": 73.56,
+        },
+        {
+            "date": dt.date(2025, 9, 4),
+            "weight": 73.10,
+        },
+    ]
+
+def test_convert_to_daily_entries_empty_dataset(client_no_creds, empty_raw_dataset):
+    assert client_no_creds.convert_to_daily_entries(empty_raw_dataset) == []
+
+def test_store_raw_data(mocker, client, sample_raw_dataset):
+    mock_open_fn = mocker.patch('google_fit.open', mocker.mock_open())
+    mock_file = mock_open_fn.return_value
+    mock_dump_fn = mocker.patch("google_fit.json.dump")
+
+    client.store_raw_data(sample_raw_dataset)
+
+    mock_open_fn.assert_called_once_with(RAW_DATA_FILE_PATH, "w")
+    mock_dump_fn.assert_called_once_with(sample_raw_dataset, mock_file)
+
+
+def test_load_creds_no_file(mocker, auth_client):
+    test_filename = "app/nonexisting_file.json"
+    mocker.patch("google_fit.TOKEN_FILE_PATH", test_filename)
+    file_open_fn = mocker.patch('google_fit.open', mocker.mock_open())
+    creds = auth_client.load_auth_token()
+
+    assert creds == None
+    # Function stopped before trying to open nonexisting files
+    assert not file_open_fn.called
+
+def test_load_valid_creds(mocker, auth_client, test_token_file_data):
+    mocker.patch("google_fit.os.path.exists").return_value = True
+
+    token_file_load_fn = mocker.patch('google_fit.open', mocker.mock_open(read_data=test_token_file_data))
+    creds_load_fn = mocker.patch("google_fit.Credentials.from_authorized_user_info")
+    creds_object = creds_load_fn.return_value
+
+    creds_object.valid = True
+
+    creds = auth_client.load_auth_token()
+
+    token_file_load_fn.assert_called_once_with(TOKEN_FILE_PATH)
+    token_data_as_dict = json.loads(test_token_file_data)
+    creds_load_fn.assert_called_once_with(token_data_as_dict)
+
+    assert creds.valid == True
+    assert creds == creds_object
+
+def test_load_creds_with_refresh_success(mocker, auth_client, test_token_file_data):
+    mocker.patch("google_fit.os.path.exists").return_value = True
+
+    token_file_load_fn = mocker.patch('google_fit.open', mocker.mock_open(read_data=test_token_file_data))
+    creds_load_fn = mocker.patch("google_fit.Credentials.from_authorized_user_info")
+    creds_object = creds_load_fn.return_value
+
+    creds_object.valid = False
+    creds_object.expired = True
+    creds_object.refresh_token = "abc567"
+
+    refresh_fn = mocker.patch.object(creds_object, "refresh")
+    save_token_fn = mocker.patch.object(auth_client, "save_auth_token_to_file")
+
+    creds = auth_client.load_auth_token()
+
+    refresh_fn.assert_called_once()
+    save_token_fn.assert_called_once_with(creds_object)
+    assert creds == creds_object
+
+def test_load_creds_with_refresh_fail(mocker, auth_client, test_token_file_data):
+    mocker.patch("google_fit.os.path.exists").return_value = True
+
+    token_file_load_fn = mocker.patch('google_fit.open', mocker.mock_open(read_data=test_token_file_data))
+    creds_load_fn = mocker.patch("google_fit.Credentials.from_authorized_user_info")
+    creds_object = creds_load_fn.return_value
+
+    creds_object.valid = False
+    creds_object.expired = True
+    creds_object.refresh_token = "abc567"
+
+    refresh_fn = mocker.patch.object(creds_object, "refresh")
+    refresh_fn.side_effect = Exception()
+
+    creds = auth_client.load_auth_token()
+
+    refresh_fn.assert_called_once()
+    assert creds == None
+
+def test_load_creds_no_refresh_token(mocker, auth_client, test_token_file_data):
+    mocker.patch("google_fit.os.path.exists").return_value = True
+
+    token_file_load_fn = mocker.patch('google_fit.open', mocker.mock_open(read_data=test_token_file_data))
+    creds_load_fn = mocker.patch("google_fit.Credentials.from_authorized_user_info")
+    creds_object = creds_load_fn.return_value
+
+    creds_object.valid = False
+    creds_object.expired = True
+    creds_object.refresh_token = None
+
+    creds = auth_client.load_auth_token()
+
+    assert creds == None
+
+def test_save_auth_token(mocker, auth_client):
+    test_creds_obj = mocker.Mock()
+    test_creds_as_json = json.dumps({"token": "aaa111"})
+
+    mocker.patch.object(test_creds_obj, "to_json").return_value = test_creds_as_json
+    mock_file_open_fn = mocker.patch('google_fit.open', mocker.mock_open())
+    mock_file = mock_file_open_fn.return_value
+
+    mock_token_file_path = mocker.patch('google_fit.Path').return_value
+    mock_create_token_file_dir_fn = mock_token_file_path.parent.mkdir
+
+    auth_client.save_auth_token_to_file(test_creds_obj)
+
+    mock_create_token_file_dir_fn.assert_called_once()
+    mock_file_open_fn.assert_called_once_with(TOKEN_FILE_PATH, "w")
+    mock_file.write.assert_called_once_with(test_creds_as_json)
+
